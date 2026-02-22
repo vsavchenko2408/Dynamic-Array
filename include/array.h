1b81e4ff25d07be130e2c07a947389e01e538421
Der Meister <v.savchenko2408@gmail.com>
2026-02-22 20:52:38 +0100

    add some methods

--- include/array.h
+++ include/array.h
@@ -1,26 +1,26 @@
 #ifndef ARRAY_H
 #define ARRAY_H
 #include <cstddef>
-
-
+#include <stdexcept>
 template<typename T>
 class Array
 {
     T* _array;
     size_t _size;
     public:
-    Array()
+////////////////////////////////////// deafult
+    Array() : _array(nullptr), _size(0)
     {
-        _size = 0;
-        _array = new T[_size];
+
     }
+////////////////////////////////////// param
     Array( size_t size) : _size(size)
     {
         _array = new T[_size];
     }
+////////////////////////////////////// copy 
     Array(const Array& copy)
     {
-
         if(copy._size > 0)
         {
             _size = copy._size;
@@ -36,6 +36,32 @@ class Array
             _array = nullptr;
         }
     }
+////////////////////////////////////// move constr
+    Array(Array&& move) noexcept
+    {
+        _size = move._size;
+        _array = move._array;
+        move._array = nullptr;
+        move._size = 0;
+    }
+////////////////////////////////////// move oper
+    Array& operator=(Array&& move) noexcept
+    {
+        if(this != &move)
+        {
+            delete[] _array;
+            _size = move._size;
+            _array = move._array;
+            move._array = nullptr;
+            move._size = 0;
+            return *this;
+        }
+        else
+        {
+            return *this;
+        }
+    }
+////////////////////////////////////// copy oper
     Array& operator=(const Array& copy)
     {
         if(this == &copy) return *this;
@@ -56,21 +82,21 @@ class Array
         }
         return *this;
     }
-    T& operator[](size_t index) const
+////////////////////////////////////// oper[]
+    const T& operator[](size_t index) const
     {
         return _array[index];
     }
+////////////////////////////////////// at()
     T& at(size_t index) const
     {
-        if(index <= _size-1)
+        if(index >= _size)
         {
+            throw std::out_of_range("Array::at");
+        }
         return _array[index];
-        }
-        else
-        {
-            throw (0);
-        }
     }
+////////////////////////////////////// push_back()
     void push_back(const T& obj)
     {
         T* temp = new T[_size+1];
@@ -83,15 +109,46 @@ class Array
         _array = temp;
         ++_size;
     }
-
+////////////////////////////////////// size()
     size_t size()const
     {
         return _size;
     }
+////////////////////////////////////// front()
+    T& front()
+    {
+        return _array[0];
+    }
+////////////////////////////////////// back()
+    T& back()
+    {
+        return _array[_size - 1];
+    }
+////////////////////////////////////// is_empty()
+    bool is_empty() const
+    {
+        if(_size >= 1)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+////////////////////////////////////// clear
+    void clear()
+    {
+        delete[] _array;
+        _array = nullptr;
+        _size = 0;
+    }
+////////////////////////////////////// destr
     ~Array()
     {
         delete[] _array;
     }
+//////////////////////////////////////
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main()
     arr.push_back(10);
     arr.push_back(20);
     arr.push_back(30);
-    arr.at(3) = 5;
+    arr.at(2) = 5;
     for(size_t i = 0; i < arr.size(); i++)
     {
         std::cout << arr[i] << " ";
