4df71641c44f4f6160bc6e05827402e20f0b1726
Der Meister <v.savchenko2408@gmail.com>
2026-02-24 22:36:11 +0100

    add methods for array(fill, back, swap, front etc)

--- include/array.h
+++ include/array.h
@@ -2,6 +2,8 @@
 #define ARRAY_H
 #include <cstddef>
 #include <stdexcept>
+#include <utility>
+
 template<typename T, size_t N>
 class Array
 {
@@ -9,32 +11,36 @@ class Array
     public:
     Array() = default;
 
-    Array(size_t size)
-    {
-        T arr[size];
-    }
     T& operator[](size_t index)
     {
         return arr[index];
     }
-
+    const T& operator[](size_t index) const
+    {
+        return arr[index];
+    }
     T& at(size_t index)
     {
-        if(index <= N)
-        {
-            return arr[index];
-        }
-        else
+        if(index >= N)
         {
             throw std::out_of_range("Array::at");
         }
+            return arr[index];
+    }
+    const T& at(size_t index) const
+    {
+        if(index >= N)
+        {
+            throw std::out_of_range("Array::at");
+        }
+            return arr[index];
     }
 
-    size_t size()
+    size_t size() const noexcept
     {
         return N;
     }
-    bool is_empty()
+    bool empty() const noexcept
     {
         if(N ==0)
         {
@@ -43,6 +49,48 @@ class Array
         else
         {
             return false;
+        }
+    }
+
+    T& front()
+    {
+        return arr[0];
+    }
+    const T& front() const
+    {
+        return arr[0];
+    }
+
+    T& back()
+    {
+        return arr[size()-1];
+    }
+    const T& back() const
+    {
+        return arr[size()-1];
+    }
+
+    T* data()
+    {
+        return arr;
+    }
+
+    const T* data() const
+    {
+        return arr;
+    }
+    void fill(const T& value)
+    {
+        for(size_t i = 0; i < N; i++)
+        {
+            arr[i] = value;
+        }
+    }
+    void swap(Array& other) noexcept
+    {
+        for(size_t i = 0; i < N; i++)
+        {
+            std::swap(this->arr[i], other.arr[i]);
         }
     }
 };
